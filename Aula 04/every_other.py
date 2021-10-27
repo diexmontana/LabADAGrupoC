7eb13ca59e38dd910498d17ea4995d124022df61
Diego Montana Neyra <83016584+diexmontana@users.noreply.github.com>
2021-10-27 08:50:50 -0500

    Descripción del ejercicio

--- Aula 04/every_other.py
+++ Aula 04/every_other.py
@@ -1,6 +1,7 @@
 # 2. every_other
+# Analizar la complejidad del código
 def every_other(array):
-    # Analizar la complejidad del código
+    # Suma los elementos de posición par con todos los elementos del arreglo
     for i,e in enumerate(array):# O(n^2) <- n*n 
         if (i % 2 == 0):
             for x in array:# O(n)
