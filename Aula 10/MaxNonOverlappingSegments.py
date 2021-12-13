9d1fec5a432df78c76dec69a34506bfbfdd52a4c
Diego Montana Neyra <83016584+diexmontana@users.noreply.github.com>
2021-12-12 23:46:19 -0500

    Se añadió descripción

--- Aula 10/MaxNonOverlappingSegments.py
+++ Aula 10/MaxNonOverlappingSegments.py
@@ -1,12 +1,20 @@
 # MaxNonOverlappingSegments.py
 
 def maxNonOverlappingSegments(listA, listB):
+    # Devuelve el número máximo de segmentos que no se sobreponen
+    
     segments = len(listA)
-    extremo = listB[0]
+    extremo = listB[0]# Representa el extremo del primer segmento
     for i in range(1, len(listA)):
+        # Para saber si no se sobreponen comparamos el extremo del primer segmento
+        # con el inicio del siguiente 
         if listA[i] > extremo:
             extremo = listB[i]
         else:
+            # Si se sobreponen, descontamos en uno al numero de segmentos
+            # Cuando dos segmentos se sobreponen escogemos aquel segmento
+            # cuyo extremo es numero menor, para que haya menor numero de sobreposiciones en los 
+            # siguientesciclos
             segments -= 1
             extremo = min(listB[i], extremo)
     return segments
